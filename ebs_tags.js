6e2e6fc00e2a125689b43cf39b5c527717d1e310
Aidan Feldman <aidan.feldman@gsa.gov>
2015-05-13 19:29:14 -0400

    write script to find ebs instances whose associated ec2 instances with client tags

---
+++ ebs_tags.js
@@ -0,0 +1,70 @@
+// http://aws.amazon.com/sdk-for-node-js/
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+
+var ec2 = new AWS.EC2();
+
+var findClient = function(resource) {
+  var tag;
+  for (var i = 0; i < resource.Tags.length; i++) {
+    tag = resource.Tags[i];
+    // TODO check for 'Client'
+    if (tag.Key === 'client') {
+      return tag.Value;
+    }
+  }
+
+  return undefined;
+};
+
+
+// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeVolumes-property
+var params = {
+  Filters: [
+    // {
+    //   Name: 'tag:client',
+    //   Values: [
+    //     'not tagged'
+    //   ]
+    // }
+    {
+      Name: 'attachment.status',
+      Values: [
+        'attached'
+      ]
+    }
+  ]
+};
+ec2.describeVolumes(params, function(err, data) {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    // console.dir(data, {depth: null});
+    data.Volumes.forEach(function(volume) {
+      var client = findClient(volume);
+
+      if (!client) {
+        volume.Attachments.forEach(function(attachment) {
+          var params = {
+            InstanceIds: [
+              attachment.InstanceId
+            ]
+          };
+          ec2.describeInstances(params, function(err, data) {
+            if (err) {
+              console.log(err, err.stack);
+            } else {
+              var instance = data.Reservations[0].Instances[0];
+              client = findClient(instance);
+              if (client) {
+                console.log('found for ' + volume.VolumeId);
+              } else {
+                console.log('not found for ' + volume.VolumeId);
+              }
+            }
+          });
+        });
+      }
+    });
+  }
+});
